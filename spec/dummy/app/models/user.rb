6a8678ef2fe06690bf29cb437675a04920bb4523
Mike Kelley <mike@codezombie.org>
2013-08-03 14:12:17 -0600

    cleaned up spec

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-	requires_authentication remote_auth_url: 'http://localhost:3000/users/authentication'
+	requires_authentication
 end
--- spec/dummy/spec/factories/user.rb
+++ spec/dummy/spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :user do
-    sequence(:email)      { |n| "test_#{n}@bqsoft.com" }
+    sequence(:email)      { |n| "test_#{n}@example.com" }
     password              'p@ssw0rd'
     password_confirmation 'p@ssw0rd'
   end
